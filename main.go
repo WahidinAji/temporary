98e6b3483a8f7fce3042011e38046ded5aa5c8dc
WahidinAji <a17wahidin@gmail.com>
2023-06-07 18:18:54 +0700

    add aja udah WKWK

--- main.go
+++ main.go
@@ -1,52 +1,48 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	obj := Obj()
-	obj.data = 2
-	fmt.Println(obj)
-	obj2 := CreateObj()
-	fmt.Println(obj2.data)
-	obj2.data = 2
-	fmt.Println(obj2.data)
-	obj3 := ManyObj()
-	fmt.Println(obj3)
+	NewCar()
+	NewPerson()
+	NewAnu()
 }
 
-type Car struct {
-	data int
-}
+func NewCar() *Car { return &Car{} }
+func NewPerson() *Person { return &Person{} }
+func NewAnu() *Anu { return &Anu{} }
 
-func Obj() Car {
-	return Car{data: 1}
-}
+type Car struct {}
+type Person struct{}
+type Anu struct{}
 
-func CreateObj() *Car {
-	return &Car{data: 1}
-}
 
-func ManyObj() []Car {
-	return []Car{{data: 1}, {data: 2}, {data: 3}}
-}
 
-func OneMilConcurrent() {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	wg.Add(500)
-	for i := 0; i < 500; i++ {
-		mu.Lock()
-		go func() {
-			defer wg.Done()
-			fmt.Println("Hello")
-		}()
-		mu.Unlock()
-	}
-	wg.Wait()
-}
+
+// func Obj() Car {
+// 	return Car{data: 1}
+// }
+
+// func CreateObj() *Car {
+// 	return &Car{data: 1}
+// }
+
+// func ManyObj() []Car {
+// 	return []Car{{data: 1}, {data: 2}, {data: 3}}
+// }
+
+// func OneMilConcurrent() {
+// 	var wg sync.WaitGroup
+// 	var mu sync.Mutex
+// 	wg.Add(500)
+// 	for i := 0; i < 500; i++ {
+// 		mu.Lock()
+// 		go func() {
+// 			defer wg.Done()
+// 			fmt.Println("Hello")
+// 		}()
+// 		mu.Unlock()
+// 	}
+// 	wg.Wait()
+// }
 
 // // Namun, jika objek yang Anda buat relatif besar dalam ukuran atau kompleksitasnya, atau objek tersebut perlu diakses atau dimodifikasi oleh pihak lain setelah fungsi selesai, maka mengembalikan objek sebagai pointer akan lebih efisien dalam hal penggunaan memori
 // func CreateObj() *Car {
